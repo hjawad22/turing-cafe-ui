57cdd273940fac2902c4bc97e26aa4242b5da015
Houda Jawad <houdajawad@gmail.com>
2023-05-30 09:55:55 -0700

    Add and import Reservations component to App component and Pass down props

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Reservations from "../Reservations/Reservations"
 import './App.css';
 
 class App extends Component {
@@ -33,10 +34,9 @@ componentDidMount() {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
         </div>
         <div className='resy-container'>
-          
+        <Reservations reservations={this.state.reservations}/>
         </div>
       </div>
     )
--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -1,1 +1,25 @@
 
+import React from "react";
+import "../Reservations/Reservations.css"
+import ReservationsCard from "../ReservationCard/ReservationCard"
+
+
+const Reservations = ({reservations}) => {
+    const cards = reservations.map(card => {
+        return (<ReservationsCard
+        id={card.id}
+        key={card.id}
+        name={card.name}
+        date={card.date}
+        time={card.time}
+        numberOfGuests={card.number} />)
+    })
+    
+    return(
+        <>
+        {cards}
+        </>
+    )
+}
+
+export default Reservations
