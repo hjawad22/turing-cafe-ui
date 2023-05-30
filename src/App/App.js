ea1e85ad3c4729ac6b963a9ff807d8f19800f085
Houda Jawad <houdajawad@gmail.com>
2023-05-30 08:37:11 -0700

    Add state and fetch to App file

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+constructor() {
+  super()
+
+this.state = {
+reservations: []
+}
+}
+
+componentDidMount() {
+  fetch('http://localhost:3001/api/v1/reservations')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch reservations')
+      }
+      return response.json()
+    })
+    .then(reservationData => {
+      this.setState({
+        reservations: reservationData
+      });
+      console.log(this.state.reservations)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+}
   render() {
     return (
       <div className="App">
